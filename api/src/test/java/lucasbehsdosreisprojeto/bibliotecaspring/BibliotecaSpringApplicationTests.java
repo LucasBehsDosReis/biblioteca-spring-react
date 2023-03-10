f2e8837867eac77d9096cb2e2890694f68637cd0
lucas_behs_dos_reis <lucasbehsdosreis.projetos@gmail.com>
2023-03-10 17:57:37 -0300

    starting-project
    Creating API folder in Spring and APP folder in React.js.

---
+++ api/src/test/java/lucasbehsdosreisprojeto/bibliotecaspring/BibliotecaSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package lucasbehsdosreisprojeto.bibliotecaspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BibliotecaSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
